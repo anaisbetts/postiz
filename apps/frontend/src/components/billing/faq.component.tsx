c33b9bffea8a2da90700412c8de2c2774340786f
Anaïs Betts <anais@anaisbetts.org>
2025-04-11 14:22:17 +0200

    fork

--- apps/frontend/src/components/billing/faq.component.tsx
+++ apps/frontend/src/components/billing/faq.component.tsx
@@ -25,7 +25,7 @@ const useFaqList = () => {
         isGeneral ? 'Postiz' : 'Gitroom'
       } is proudly open-source! We believe in an ethical and transparent culture, meaning that ${
         isGeneral ? 'Postiz' : 'Gitroom'
-      } will live forever. You can check out the entire code or use it for personal projects. To view the open-source repository, <a href="https://github.com/gitroomhq/postiz-app" target="_blank" style="text-decoration: underline;">click here</a>.`,
+      } will live forever. You can check out the entire code or use it for personal projects. To view the open-source repository, <a href="https://github.com/anaisbetts/postiz" target="_blank" style="text-decoration: underline;">click here</a>.`,
     },
     {
       title: 'What are channels?',
